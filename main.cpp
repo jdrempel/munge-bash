8ed834d88f9bf337c1d03612e89f73ded771fda0
Jeremy Rempel <jeremy.rempel@usask.ca>
2022-04-18 19:56:51 -0600

    add imgui

--- main.cpp
+++ main.cpp
@@ -1,206 +1,324 @@
-#include <gtkmm.h>
+//
+// Created by jeremy on 2022-04-18.
+//
 
-#include <cctype>
-#include <filesystem>
-#include <fstream>
-#include <functional>
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include <chrono>
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <iostream>
-#include <regex>
-#include <string>
+#include <thread>
 
-bool check(const std::string &prompt, const std::string &def)
-{
-    std::string input;
-    std::string options;
-    if (def == "Y" || def == "y") {
-        options = "(Y/n)";
-    } else {
-        options = "(y/N)";
-    }
-    std::cout << prompt << " " << options;
-    std::getline(std::cin, input);
+const char *vertexShaderSrc = R"(
+#version 450
 
-    if (input.empty())
-        input = def;
+layout (location = 0) in vec2 aPos;
 
-    if (input == "Y" || input == "y")
-        return true;
-
-    return false;
+void main() {
+    gl_Position = vec4(aPos, 1, 1);
 }
 
-bool isValidLanguage(const std::string &lang)
-{
-    auto languages = std::regex("^(de|en|es|fr|it|ja|uk)?$", std::regex::icase);
-    return std::regex_match(lang, languages);
+)";
+
+const char *fragmentShaderSrc = R"(
+#version 450
+
+layout (location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(1, 0, 0, 1);
 }
 
-std::string getLongLanguageName(std::string &lang)
-{
-    if (lang == "DE") return "GERMAN";
-    if (lang == "EN") return "ENGLISH";
-    if (lang == "ES") return "SPANISH";
-    if (lang == "FR") return "FRENCH";
-    if (lang == "IT") return "ITALIAN";
-    if (lang == "JA") return "JAPANESE";
-    if (lang == "UK") return "UK";
+)";
+
+void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                      GLsizei length, GLchar const *message,
+                      void const *user_param) {
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+        return;
+
+    auto const src_str = [source]() {
+        switch (source) {
+            case GL_DEBUG_SOURCE_API:
+                return "API";
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+                return "WINDOW SYSTEM";
+            case GL_DEBUG_SOURCE_SHADER_COMPILER:
+                return "SHADER COMPILER";
+            case GL_DEBUG_SOURCE_THIRD_PARTY:
+                return "THIRD PARTY";
+            case GL_DEBUG_SOURCE_APPLICATION:
+                return "APPLICATION";
+            case GL_DEBUG_SOURCE_OTHER:
+                return "OTHER";
+            default:
+                return "UNKNOWN SOURCE";
+        }
+    }();
+
+    auto const type_str = [type]() {
+        switch (type) {
+            case GL_DEBUG_TYPE_ERROR:
+                return "ERROR";
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+                return "DEPRECATED_BEHAVIOR";
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+                return "UNDEFINED_BEHAVIOR";
+            case GL_DEBUG_TYPE_PORTABILITY:
+                return "PORTABILITY";
+            case GL_DEBUG_TYPE_PERFORMANCE:
+                return "PERFORMANCE";
+            case GL_DEBUG_TYPE_MARKER:
+                return "MARKER";
+            case GL_DEBUG_TYPE_OTHER:
+                return "OTHER";
+            default:
+                return "UNKNOWN TYPE";
+        }
+    }();
+
+    auto const severity_str = [severity]() {
+        switch (severity) {
+            case GL_DEBUG_SEVERITY_NOTIFICATION:
+                return "NOTIFICATION";
+            case GL_DEBUG_SEVERITY_LOW:
+                return "LOW";
+            case GL_DEBUG_SEVERITY_MEDIUM:
+                return "MEDIUM";
+            case GL_DEBUG_SEVERITY_HIGH:
+                return "HIGH";
+            default:
+                return "UNKNOWN SEVERITY";
+        }
+    }();
+    std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id
+              << ": " << message << '\n';
 }
 
-bool isValidPlatform(const std::string &platform)
+int main(int argc, char *argv[])
 {
-    auto platforms = std::regex("^(pc|ps2|xbox)?$", std::regex::icase);
-    return std::regex_match(platform, platforms);
-}
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-int main()
-{
-    /*
-     * - Check the current directory
-     * - See if the BF2 directory has been set and if not, prompt for it
-     * - Prompt for:
-     *   - common
-     *   - load
-     *   - shell
-     *   - sides
-     *     - EVERYTHING
-     *     - NOTHING
-     *     - <side> (list dirs in data_ABC/Sides)
-     *     - gcw
-     *     - cw
-     *   - sound
-     *   - worlds
-     *     - EVERYTHING
-     *     - NOTHING
-     *     - <world> (list dirs in data_ABC/Worlds)
-     * - Run the scripts
-     * - Copy results to BF2 dir
+    auto window = glfwCreateWindow(600, 800, "VisualMunge for Linux", nullptr, nullptr);
+    if (!window)
+        throw std::runtime_error("Error creating glfw window");
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (!gladLoaderLoadGL())
+        throw std::runtime_error("Error initializing glad");
+
+    /**
+     * Initialize ImGui
      */
-    namespace fs = std::filesystem;
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 450 core");
+    ImGui::StyleColorsClassic();
 
-    auto current_path = fs::current_path();
-    // TODO re-enable this check
-    // if (current_path.parent_path() != "_BUILD") {
-    //     std::cerr << "Parent directory is " << current_path.stem() << ". Please check that visualmunge is placed"
-    //                                                                   " in data_ABC/_BUILD before running it."
-    //                                                                   << std::endl;
-    //     exit(1);
-    // }
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(message_callback, nullptr);
 
-    auto munge_path = current_path / "munge.sh";
-    if (!fs::exists(munge_path)) {
-        std::cerr << "File munge.sh not found. Please check that the munge-bash files are placed correctly in data_ABC/"
-                     "_BUILD/ before running visualmunge." << std::endl;
-        exit(1);
+    /**
+     * Compile shader
+     */
+    int success;
+    char infoLog[512];
+    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, 0);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "Vertex shader compilation failed:" << std::endl;
+        std::cerr << infoLog << std::endl;
+        return 0;
     }
 
-    std::string bf2_path_str;
-    auto bf2_storage_path = current_path / ".swbf2";
-    if (fs::exists(bf2_storage_path)) {
-        // read the file to get the absolute path of BF2 installation
-        std::ifstream bf2_file;
-        bf2_file.open(bf2_storage_path, std::ios::in);
-        if (!bf2_file) {
-            std::cerr << "File .swbf2 not found." << std::endl;
-            exit(1);
+    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, 0);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cerr << "Fragment shader compilation failed:" << std::endl;
+        std::cerr << infoLog << std::endl;
+        return 0;
+    }
+
+    auto program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(program, 512, nullptr, infoLog);
+        std::cerr << "Shader linking failed:" << std::endl;
+        std::cerr << infoLog << std::endl;
+        return 0;
+    }
+
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
+
+    /**
+     * Create vertex array and buffers
+     */
+    GLuint vao;
+    glCreateVertexArrays(1, &vao);
+
+    glEnableVertexArrayAttrib(vao, 0);
+    glVertexArrayAttribFormat(vao, 0, 2, GL_FLOAT, GL_FALSE,
+                              offsetof(glm::vec2, x));
+
+    glVertexArrayAttribBinding(vao, 0, 0);
+
+    GLuint vbo;
+    glCreateBuffers(1, &vbo);
+
+    GLuint ibo;
+    glCreateBuffers(1, &ibo);
+
+    glBindVertexArray(vao);
+    glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(glm::vec2));
+    glVertexArrayElementBuffer(vao, ibo);
+    glUseProgram(program);
+    glClearColor(1, 1, 1, 1);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("Quit", "Ctrl+Q")) {
+                    break;
+                }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Tools")) {
+                if (ImGui::MenuItem("Set SWBF2 path...")) {
+                    // Open file browser to get SWBF2 path
+                }
+                if (ImGui::MenuItem("Options...")) {
+                    // Open options menu
+                }
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Help")) {
+                if (ImGui::MenuItem("Documentation")) {
+                    // Open window with basic help text
+                }
+                if (ImGui::MenuItem("About")) {
+                    // Open window with developer info and a hyperlink to the github page
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
         }
-        std::getline(bf2_file, bf2_path_str);
-        bf2_file.close();
-    } else {
-        // prompt for the path, create the file, store the path
-        std::cout << "Please enter the absolute path to your SWBF2 GameData directory:" << std::endl;
-        std::getline(std::cin, bf2_path_str);
-        std::ofstream bf2_file;
-        bf2_file.open(bf2_storage_path, std::ios::out);
-        if (!bf2_file) {
-            std::cerr << "File .swbf2 could not be created." << std::endl;
-            exit(1);
+
+        static char world_code_buffer[5] = {0};
+        static char world_name_buffer[33] = {0};
+        static char world_desc_buffer[2049] = {0};
+        static bool space_map = false;
+        static bool conquest, ctf2, ctf1, assault;
+        static ImGuiWindowFlags flags =
+                ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoSavedSettings |
+                ImGuiWindowFlags_NoCollapse;
+
+        const ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        auto workSize = ImVec2{viewport->WorkSize.x, viewport->WorkSize.y * 2/3};
+        ImGui::SetNextWindowSize(workSize);
+        ImGui::SetNextWindowBgAlpha(1);
+
+        bool yes = true;
+        if (ImGui::Begin("Create New World", &yes, flags)) {
+            {
+                ImGui::Text("3-Letter World Name");
+                ImGui::SameLine();
+                ImGui::SetNextItemWidth(3 * ImGui::GetFontSize());
+                ImGui::InputTextWithHint("##world-code", "ABC", world_code_buffer, 4);
+                ImGui::SameLine();
+                ImGui::Text("Full World Name");
+                ImGui::SameLine();
+                ImGui::SetNextItemWidth(-1);
+                ImGui::InputTextWithHint("##world-name", "Snazzy ModWorld", world_name_buffer, 32);
+            }
+            {
+                ImGui::Text("World Description (3 lines max)");
+                ImGui::SetNextItemWidth(-1);
+                ImGui::InputTextMultiline("##world-desc", world_desc_buffer, 2048);
+            }
+            {
+                ImGui::Checkbox("Space Map", &space_map);
+                ImGui::SameLine(ImGui::GetWindowWidth()/2);
+                ImGui::BeginGroup();
+                {
+                    ImGui::Checkbox("Conquest", &conquest);
+                    ImGui::Checkbox("2-Flag CTF", &ctf2);
+                    ImGui::Checkbox("1-Flag CTF", &ctf1);
+                    ImGui::Checkbox("Assault", &assault);
+                }
+                ImGui::EndGroup();
+            }
+            if (ImGui::Button("Create World")) {
+                // Munge!
+            }
+            ImGui::End();
         }
-        bf2_file << bf2_path_str << std::endl;
-        bf2_file.close();
+
+        auto mungePos = ImVec2{viewport->WorkPos};
+        ImGui::SetNextWindowPos(mungePos);
+        ImGui::SetNextWindowSize(workSize);
+        ImGui::SetNextWindowBgAlpha(1);
+        if (ImGui::Begin("Munge", &yes, flags)) {
+
+            ImGui::End();
+        }
+
+        auto consolePos = ImVec2{viewport->WorkPos.x, viewport->WorkSize.y * 2/3};
+        auto consoleSize = ImVec2{viewport->WorkSize.x, viewport->WorkSize.y / 3};
+        ImGui::SetNextWindowPos(consolePos);
+        ImGui::SetNextWindowSize(consoleSize);
+        ImGui::SetNextWindowBgAlpha(1);
+        if (ImGui::Begin("Console", &yes, flags)) {
+
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 
-    if (bf2_path_str.empty()) {
-        std::cerr << "Invalid SWBF2 GameData directory (empty)." << std::endl;
-        exit(1);
-    }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-    std::string args;
-
-    std::string platform;
-    std::cout << "Platform (PC|ps2|xbox): " << std::flush;
-    std::getline(std::cin, platform);
-    if (!isValidPlatform(platform)) {
-        std::cerr << "Invalid platform entered." << std::endl;
-        exit(1);
-    }
-    if (platform.empty()) {
-        platform = "pc";
-    }
-    std::transform(
-            platform.begin(),
-            platform.end(),
-            platform.begin(),
-            std::ptr_fun<int, int>(std::toupper)
-            );
-
-    args += " --platform " + platform;
-
-    std::string language;
-    std::cout << "Language (EN|de|es|fr|it|ja|uk): " << std::flush;
-    std::getline(std::cin, language);
-    if (!isValidLanguage(language)) {
-        std::cerr << "Invalid language entered." << std::endl;
-        exit(1);
-    }
-    if (language.empty()) {
-        language = "ENGLISH";
-    }
-    std::transform(
-            language.begin(),
-            language.end(),
-            language.begin(),
-            std::ptr_fun<int, int>(std::toupper)
-            );
-    language = getLongLanguageName(language);
-
-    args += " --language " + language;
-
-    bool munge_common = check("Munge common", "Y");
-    bool munge_load = check("Munge load", "N");
-    bool munge_localize = check("Munge localize", "N");
-    bool munge_movies = check("Munge movies", "N");
-    bool munge_shell = check("Munge shell", "N");
-    bool munge_sound = check("Munge sound", "N");
-
-    // TODO localize sides worlds
-
-    if (munge_common) {
-        args += " --common";
-    }
-
-    if (munge_load) {
-        args += " --load";
-    }
-
-    if (munge_localize) {
-        args += " --localize";
-    }
-
-    if (munge_movies) {
-        args += " --movies";
-    }
-
-    if (munge_shell) {
-        args += " --shell";
-    }
-
-    if (munge_sound) {
-        args += " --sound";
-    }
-
-    std::string exe = "./munge.sh" + args;
-
-    std::system(exe.c_str());
-
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
